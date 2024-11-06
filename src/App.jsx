af5da618c74b51b6d3779f02b4c03f6d8d88c1ae
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-11-06 23:05:41 +0530

    :Sparkles: Added router in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,6 @@
-import "./App.css";
-
-import Main from "./components/Main";
-import SidebarLower from "./components/SidebarLower";
-import SidebarUpper from "./components/SidebarUpper";
-
+import Router from "./Router";
 function App() {
-  return (
-    // Full App
-    <div className="app">
-      {/* Left Sidebar  */}
-      <section className="sidebar">
-        {/* Left Sidebar Upper Portion */}
-        <section className="sidebar--upper">
-          <SidebarUpper />
-        </section>
-        {/* Left Sidebar Lower Portion */}
-        <section className="sidebar--lower">
-          <SidebarLower />
-        </section>
-      </section>
-      {/* Right Side Main Content */}
-      <main className="main-content">
-        <Main />
-      </main>
-    </div>
-  );
+  return <Router />;
 }
 
 export default App;
