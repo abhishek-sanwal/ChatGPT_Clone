8e9a7c03e548b8c5bb6105b38e0f4e86e5e68978
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-11-06 23:09:42 +0530

    :sparkles: Added typing effect

--- src/components/TypewriterComponent.jsx
+++ src/components/TypewriterComponent.jsx
@@ -1,18 +1,45 @@
-import { Cursor, useTypewriter } from "react-simple-typewriter";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { nightOwl } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { useTypewriter } from "react-simple-typewriter";
 
 function TypewriterComponent({ data }) {
-  console.log(data);
   const [text] = useTypewriter({
-    words: ["Hello", "From", "Typewriter", "Hook!"],
-    typeSpeed: 30,
-    delaySpeed: 160,
+    words: [data],
+    typeSpeed: 10,
   });
 
   return (
-    <md-block>
-      {text}
-      <Cursor cursorColor="red" />
-    </md-block>
+    <div>
+      {/* Render Markdown with syntax highlighting */}
+      <ReactMarkdown
+        children={text}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            // If the code block is inline
+            if (inline) {
+              return (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            }
+
+            // If it's a block code
+            const language = className?.replace("language-", "");
+            return (
+              <SyntaxHighlighter
+                style={nightOwl}
+                language={language}
+                children={String(children).replace(/\n$/, "")}
+                {...props}
+              />
+            );
+          },
+        }}
+      />
+    </div>
   );
 }
 
