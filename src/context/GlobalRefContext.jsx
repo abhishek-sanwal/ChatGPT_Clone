5faadca0d869c214afd37e0b774fa63cde10d1b3
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-11-07 17:48:35 +0530

    :zap: Moved side effects to this file from Main

--- src/context/GlobalRefContext.jsx
+++ src/context/GlobalRefContext.jsx
@@ -1,10 +1,32 @@
-import { createContext, useContext, useRef } from "react";
+import { createContext, useContext, useEffect, useRef } from "react";
+
+import { useChatContext } from "./ChatContext";
 
 const globalRefContext = createContext();
 
 function GlobalRefContextProvider({ children }) {
   const chatsRef = useRef();
   const inputRef = useRef();
+  const { messages } = useChatContext();
+
+  // Should run only on mount
+  useEffect(function () {
+    // Default focus should be on input element
+    inputRef.current?.focus();
+  }, []);
+
+  useEffect(
+    () =>
+      async function () {
+        // Scroll to latest question and answer automatically on mount
+        //  or when there is a change in messages
+        chatsRef.current?.scrollIntoView({
+          block: "end",
+          behavior: "smooth",
+        });
+      },
+    [messages]
+  );
 
   return (
     <globalRefContext.Provider
