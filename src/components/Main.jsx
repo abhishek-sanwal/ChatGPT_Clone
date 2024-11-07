170851ab5329fa66645b0b9c82e60e134c1b4151
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-11-07 17:55:54 +0530

    :sparkles: Moved side effects from component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,7 @@
-import { useEffect, useRef } from "react";
-
 import ChatBox from "./ChatBox";
 import ChatFooter from "./ChatFooter";
-import { useChatContext } from "../context/ChatContext";
-import { useGlobalRefContext } from "../context/GlobalRefContext";
 
 function Main() {
-  const { messages } = useChatContext();
-  const { inputRef, chatsRef } = useGlobalRefContext();
-
-  // Should run only on mount
-  useEffect(function () {
-    // Default focus should be on input element
-    inputRef.current?.focus();
-  }, []);
-
-  useEffect(
-    () =>
-      async function () {
-        // Scroll to latest question and answer automatically on mount
-        //  or when there is a change in messages
-        chatsRef.current?.scrollIntoView({
-          block: "end",
-          behavior: "smooth",
-        });
-      },
-    [messages]
-  );
-
   return (
     <>
       {/* Displays ALl previous chats messages */}
